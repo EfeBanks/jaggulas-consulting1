da373358da15d76e578de93dfc8c041eefcaa314
Efe Bankole <104216495+EfeBanks@users.noreply.github.com>
2024-12-05 16:12:26 +0100

    update

--- src/components/JCLocation.jsx
+++ src/components/JCLocation.jsx
@@ -11,7 +11,7 @@ export default function JCLocation() {
           <div className="container">
           <div className="row mx-sm-5 mx-2 d-flex justify-content-center g-3 text-light">
             <div className="col-lg first-cont py-5 rounded">
-            <div className="p-3"> 
+            <div className="py-3"> 
                   <h5 className="text-center border-bottom border-secondary pb-2 mx-2">South Africa</h5>
                   <div className="cont-inner p-2">
                   <div className="address my-3">
@@ -33,7 +33,7 @@ export default function JCLocation() {
             </div>
 
             <div className="col-lg middle-cont mx-4 py-5 rounded">
-            <div className="p-3"> 
+            <div className="py-3"> 
                   <h5 className="text-center border-bottom border-secondary pb-2 mx-2">South Africa</h5>
                   <div className="cont-inner p-2">
                   <div className="address my-3">
@@ -55,7 +55,7 @@ export default function JCLocation() {
             </div>
 
             <div className="col-lg second-cont py-5 rounded-3">
-            <div className="p-3">
+            <div className="py-3">
                   <h5 className=" text-center border-bottom border-secondary pb-2 mx-2">India</h5>
                   <div className="cont-inner p-2 ">
                   <div className="address my-3">
